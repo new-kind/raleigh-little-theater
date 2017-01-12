7971524e9790fcf56ae7369ea4ea3f2e21258841
Josh Mobley <josh@newkind.com>
2017-01-12 11:20:11 -0500

    fixing front-page template

---
+++ wp-content/themes/rlt/front-page.php
@@ -0,0 +1,6 @@
+<?php
+
+    $context = Timber::get_context();
+    $post = new TimberPost();
+    $context['post'] = $post;
+    Timber::render( array( 'pages/front-page.twig', 'pages/page.twig' ), $context );
