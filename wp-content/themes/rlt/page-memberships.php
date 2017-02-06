a0a6cfe184354b694b70503b61f3f1f878e2257c
Brent Brinkley <brentbrinkley@gmail.com>
2017-02-06 12:27:54 -0500

    Corrected rendering

--- wp-content/themes/rlt/page-memberships.php
+++ wp-content/themes/rlt/page-memberships.php
@@ -3,4 +3,4 @@
     $context = Timber::get_context();
     $post = new TimberPost();
     $context['post'] = $post;
-    Timber::render( array( 'pages/page-' . $post->post_name . '.twig', 'pages/page-our-staff.twig' ), $context );
+    Timber::render( array( 'pages/page-' . $post->post_name . '.twig', 'pages/page-memberships.twig' ), $context );
