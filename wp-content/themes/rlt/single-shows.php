73e292fea22cdc139f92eb406e6a2f3dad73b6a4
Josh Mobley <jmob1986@gmail.com>
2017-02-21 15:36:47 -0500

    news templates and styling

--- wp-content/themes/rlt/single-shows.php
+++ wp-content/themes/rlt/single-shows.php
@@ -1,6 +1,4 @@
  <?php   
     $context = Timber::get_context();
     $context['post'] = new TimberPost();
-    $context['navbar'] = array();
-    $context['foo'] = 'bar';
     Timber::render( "singles/shows.twig", $context );
--- wp-content/themes/rlt/single.php
+++ wp-content/themes/rlt/single.php
@@ -1,4 +1,4 @@
  <?php   
     $context = Timber::get_context();
     $context['post'] = new TimberPost();
-    Timber::render( "singles/classes.twig", $context );
+    Timber::render( "singles/post.twig", $context );
