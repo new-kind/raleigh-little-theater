9dfafa9c8a2cce8137c44ee4934198db19124d1a
Josh Mobley <jmob1986@gmail.com>
2017-02-23 17:42:56 -0500

    fixing reference to age groups in acf

--- wp-content/themes/rlt/template-directory.php
+++ wp-content/themes/rlt/template-directory.php
@@ -6,12 +6,14 @@
     $context['post'] = $post;
     $args = array(
         'post_type' => 'classes',
+        'orderby' => "title",
+        'order' => "ASC",
         'posts_per_page' => -1
     );
     $context['posts'] = Timber::get_posts($args);
     $context['locations'] = Timber::get_terms('locations');
     $context['types'] = Timber::get_terms('types');
-    $context["acf"] = get_field_objects($context["posts"][0]->ID);
+    $context['age_groups'] = get_field_object('field_588b950ddcb5c');
     $context['kids_ages'] = get_field_object('field_588b93ffd574a');
     $context['teens_ages'] = get_field_object('field_588b956071854');
     Timber::render( array( 'pages/page-class-directory.twig' ), $context );
