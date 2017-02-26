a9efb00280a2717058e6180bc81a8e1fe01946a7
Josh Mobley <jmob1986@gmail.com>
2017-02-25 19:36:02 -0500

    setting homepage news feed to 6

--- wp-content/themes/rlt/front-page.php
+++ wp-content/themes/rlt/front-page.php
@@ -5,7 +5,7 @@
     $context['post'] = $post;
     $args = array(
         'post_type' => 'post',
-        'posts_per_page' => 8
+        'posts_per_page' => 6
     );
     $context['news'] = Timber::get_posts($args);
     Timber::render( array( 'pages/front-page.twig', 'pages/page.twig' ), $context );
