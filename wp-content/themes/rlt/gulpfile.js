aa9125d5addd2dac4118941a8c0e27ee199b1723
Will Langley <wlangley@medthink.com>
2017-09-22 16:26:21 -0400

    events/shows dates, template tweaks, classes change to education

--- wp-content/themes/rlt/gulpfile.js
+++ wp-content/themes/rlt/gulpfile.js
@@ -30,7 +30,8 @@ var autoprefixer = require('gulp-autoprefixer');
 
         // initialize browser-sync server on localhost:3000
         browserSync.init({
-             proxy: "localhost:8888/raleigh-little-theater"
+             //proxy: "localhost:8888/raleigh-little-theater"
+             proxy: "raleigh-little-theater:8888"
         });
     });
 
